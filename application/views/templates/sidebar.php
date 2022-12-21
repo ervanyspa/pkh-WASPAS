ba550aa33463af236715927e6196803326003a18
ipaaaa <ti.rifanurh@gmail.com>
2022-12-21 22:44:43 +0700

    dashboard

---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,32 @@
+
+      <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html"> <img alt="image" src="assets/img/logo.png" class="header-logo" /> <span
+                class="logo-name">PKH</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Main</li>
+            <li class="dropdown active">
+              <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                  data-feather="briefcase"></i><span>Master Data</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="widget-chart.html">Periode Graduasi</a></li>
+                <li><a class="nav-link" href="widget-data.html">Data Penerima Bantuan</a></li>
+                <li><a class="nav-link" href="widget-data.html">Data Kriteria dan Bobot</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="command"></i><span>Proses Seleksi</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="chat.html">Data Peserta Graduasi</a></li>
+              </ul>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i data-feather="mail"></i><span>Laporan Hasil Seleksi</span></a></li>
+          </ul>
+        </aside>
+      </div>
