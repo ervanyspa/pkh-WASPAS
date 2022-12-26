d60a5a471e5e5b1a8cdc3f8888064d78464aca33
ipaaaa <ti.rifanurh@gmail.com>
2022-12-26 18:19:33 +0700

    base url

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,67 +3,65 @@
 
 
 <!-- index.html  21 Nov 2019 03:44:50 GMT -->
+
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>PKH WASPAS</title>
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/app.min.css">
-	
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/bundles/datatables/datatables.min.css">
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/components.css">
-  <!-- Custom style CSS -->
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/css/custom.css">
-  <link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url()?>assets/img/favicon.ico' />
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+	<title>PKH WASPAS</title>
+	<!-- General CSS Files -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/app.min.css">
+
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/bundles/datatables/datatables.min.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+	<!-- Template CSS -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/components.css">
+	<!-- Custom style CSS -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/custom.css">
+	<link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url() ?>assets/img/favicon.ico' />
 </head>
 
 <body>
-  <div class="loader"></div>
-  <div id="app">
-    <div class="main-wrapper main-wrapper-1">
-      <div class="navbar-bg"></div>
-      <nav class="navbar navbar-expand-lg main-navbar sticky">
-        <div class="form-inline mr-auto">
-          <ul class="navbar-nav mr-3">
-            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg
+	<div class="loader"></div>
+	<div id="app">
+		<div class="main-wrapper main-wrapper-1">
+			<div class="navbar-bg"></div>
+			<nav class="navbar navbar-expand-lg main-navbar sticky">
+				<div class="form-inline mr-auto">
+					<ul class="navbar-nav mr-3">
+						<li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg
 									collapse-btn"> <i data-feather="align-justify"></i></a></li>
-            <li><a href="#" class="nav-link nav-link-lg fullscreen-btn">
-                <i data-feather="maximize"></i>
-              </a></li>
-            <li>
-              <form class="form-inline mr-auto">
-                <div class="search-element">
-                  <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="200">
-                  <button class="btn" type="submit">
-                    <i class="fas fa-search"></i>
-                  </button>
-                </div>
-              </form>
-            </li>
-          </ul>
-        </div>
-        <ul class="navbar-nav navbar-right">
-          <li class="dropdown"><a href="#" data-toggle="dropdown"
-              class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="<?php echo base_url()?>assets/img/user.png"
-                class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
-            <div class="dropdown-menu dropdown-menu-right pullDown">
-              <div class="dropdown-title">Hello Sarah Smith</div>
-              <a href="profile.html" class="dropdown-item has-icon"> <i class="far
+						<li><a href="#" class="nav-link nav-link-lg fullscreen-btn">
+								<i data-feather="maximize"></i>
+							</a></li>
+						<li>
+							<form class="form-inline mr-auto">
+								<div class="search-element">
+									<input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="200">
+									<button class="btn" type="submit">
+										<i class="fas fa-search"></i>
+									</button>
+								</div>
+							</form>
+						</li>
+					</ul>
+				</div>
+				<ul class="navbar-nav navbar-right">
+					<li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="<?php echo base_url() ?>assets/img/user.png" class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
+						<div class="dropdown-menu dropdown-menu-right pullDown">
+							<div class="dropdown-title">Hello Sarah Smith</div>
+							<a href="profile.html" class="dropdown-item has-icon"> <i class="far
 										fa-user"></i> Profile
-              </a> <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
-                Activities
-              </a> <a href="#" class="dropdown-item has-icon"> <i class="fas fa-cog"></i>
-                Settings
-              </a>
-              <div class="dropdown-divider"></div>
-              <a href="auth-login.html" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
-                Logout
-              </a>
-            </div>
-          </li>
-        </ul>
-      </nav>
-      
+							</a> <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
+								Activities
+							</a> <a href="#" class="dropdown-item has-icon"> <i class="fas fa-cog"></i>
+								Settings
+							</a>
+							<div class="dropdown-divider"></div>
+							<a href="auth-login.html" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
+								Logout
+							</a>
+						</div>
+					</li>
+				</ul>
+			</nav>
