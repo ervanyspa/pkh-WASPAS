916267cb9ba38f3ff9d50711614417f051ef4cdd
ipaaaa <ti.rifanurh@gmail.com>
2023-01-29 16:58:57 +0700

    favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,7 +20,7 @@
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/components.css">
 	<!-- Custom style CSS -->
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/custom.css">
-	<link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url() ?>assets/img/favicon.ico' />
+	<link rel='shortcut icon' type='image/x-icon' href='<?= base_url() ?>assets/img/image-gallery/PKH.png' />
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
