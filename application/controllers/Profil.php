62a62b8b3ef8dc54968c076f00ad058280fccc05
user <ti.ervany@gmail.com>
2023-02-04 20:10:35 +0700

    edit profil, ubah password profil

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -30,6 +30,140 @@ class Profil extends CI_Controller
 
 	public function update_profil()
 	{
-		
+		if (isset($_POST['submit'])) {
+			$nama       = $this->input->post('nama');
+			$alamat     = $this->input->post('alamat');
+			$nohp   	= $this->input->post('nohp');
+			$alamat   	= $this->input->post('alamat');
+			$username   = $this->input->post('username');
+			$foto		= $_FILES['foto']['name'];
+
+			if (!empty($foto)) { //gambar tdk kosong
+				$config['upload_path'] 		= './assets/img/uploads/';
+				$config['allowed_types'] 	= 'jpg|jpeg|png';
+				$config['file_name']		= 'usr' . date('dmyhis');
+				$this->load->library('upload', $config);
+
+				if (!$this->upload->do_upload('foto')) { //gambar tidak terupload
+					$data2 = array(
+
+						'nama'      => $nama,
+						'alamat'    => $alamat,
+						'nohp'		=> $nohp,
+						'alamat'    => $alamat,
+						'username'	=> $username,
+					);
+					$this->session->set_flashdata(
+						'berhasil_profil',
+						'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+										<script type ="text/JavaScript">  
+										swal("Gambar Gagal Diunggah","Data Profil Berhasil Diubah, Gambar Gagal Diunggah","warning"); 
+										</script>'
+					);
+				} else { //gambar terupload
+					$this->db->select('foto')->from('petugas')->where('id_petugas', $id);
+					$query = $this->db->get();
+					if ($query->num_rows() > 0) { //hapus gambar sebelumnya
+						$img_name = $query->row()->foto;
+						unlink("./assets/img/uploads/" . $img_name);
+					}
+					//mengganti gambar
+					$foto = $this->upload->data('file_name');
+					$this->db->set('foto', $foto);
+					$data2 = array(
+						'nama'      => $nama,
+						'alamat'    => $alamat,
+						'nohp'		=> $nohp,
+						'alamat'    => $alamat,
+						'username'	=> $username,
+						'foto'    	=> $foto,
+					);
+					$this->session->set_flashdata(
+						'berhasil_profil',
+						'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+										<script type ="text/JavaScript">  
+										swal("Sukses","Data Profil Berhasil Diubah","success"); 
+										</script>'
+					);
+				}
+			} else { //edit tanpa ganti gambar
+				$data2 = array(
+					'nama'      => $nama,
+					'alamat'    => $alamat,
+					'nohp'		=> $nohp,
+					'alamat'    => $alamat,
+					'username'	=> $username,
+				);
+				$this->session->set_flashdata(
+					'berhasil_profil',
+					'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+									<script type ="text/JavaScript">  
+									swal("Sukses","Data Profil Berhasil Diubah","success"); 
+									</script>'
+				);
+			}
+
+			$where2 = array('id_petugas' => $this->session->userdata('id_petugas'));
+			$this->db->update('petugas', $data2, $where2);
+			redirect('profil');
+		}
+		if(isset($_POST['cancle'])) {
+			$this->session->set_flashdata(
+				'berhasil_profil',
+				'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+								<script type ="text/JavaScript">  
+								swal("Gagal","Data Profil Batal Diubah","info"); 
+								</script>'
+			);
+
+			redirect('profil');
+		}
+	}
+
+	public function ubah_password()
+	{
+		if (isset($_POST['submit'])) {
+			$data = $this->Model_user->get_user_by_id($this->session->userdata('id_petugas'))->row_array();
+			$password_lama       = $this->input->post('password_lama');
+			$password_baru     = password_hash($this->input->post('password_baru'), PASSWORD_DEFAULT);
+			if (password_verify($password_lama, $data['password'])) {
+				$data2 = array(
+					'password'      => $password_baru,
+				);
+				$where2 = array('id_petugas' => $this->session->userdata('id_petugas'));
+				$this->db->update('petugas', $data2, $where2);
+				$this->session->set_flashdata(
+					'berhasil_profil',
+					'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+										<script type ="text/JavaScript">  
+										swal("Sukses","Password Berhasil Diubah","success"); 
+										</script>'
+				);
+				redirect('profil');
+			} else {
+				$this->session->set_flashdata(
+					'berhasil_profil',
+					'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+										<script type ="text/JavaScript">  
+										swal("Sukses","Password Anda Salah","error"); 
+										</script>'
+				);
+				redirect('profil');
+			}
+		}
+		if (isset($_POST['cancel'])) {
+			$this->session->set_flashdata(
+				'berhasil_profil',
+				'<script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+								<script type ="text/JavaScript">  
+								swal("Gagal","Password Batal Diubah","info"); 
+								</script>'
+			);
+
+			redirect('profil');
+		}
+
+
+
 	}
 }
--- application/views/profil.php
+++ application/views/profil.php
@@ -1,3 +1,4 @@
+<?php echo $this->session->flashdata('berhasil_profil') ?>
 <div class="main-content">
 	<section class="section">
 		<div class="section-body">
@@ -52,12 +53,9 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<h5 class="modal-title" id="formModal">Edit Profil</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
 				</div>
 				<div class="modal-body">
-					<form class="">
+					<form action="<?= base_url() ?>profil/update_profil" method="post" enctype="multipart/form-data">
 						<div class="form-group">
 							<label>Nama</label>
 							<div class="input-group">
@@ -89,8 +87,8 @@
 							</div>
 						</div>
 						<div class="form-group align-right">
-							<button type="button" class="btn btn-secondary waves-effect mr-1">Batal</button>
-							<button type="button" class="btn btn-primary waves-effect">Simpan</button>
+							<button type="cancle" name="cancle" class="btn btn-secondary waves-effect mr-1">Batal</button>
+							<button type="submit" name="submit" class="btn btn-primary waves-effect">Simpan</button>
 						</div>
 					</form>
 				</div>
@@ -109,28 +107,38 @@
 					</button>
 				</div>
 				<div class="modal-body">
-					<form class="">
+					<form action="<?= base_url() ?>profil/ubah_password" method="post" enctype="multipart/form-data">
 						<div class="form-group">
 							<label>Password Lama</label>
 							<div class="input-group">
-								<input type="text" class="form-control" placeholder="" name="nama">
+								<input type="password" id="password" class="form-control" placeholder="" name="password_lama">
+								<div class="input-group-append">
+										<div class="input-group-text"><i class="fas fa-eye " id="eyeIcon"></i></div>
+									</div>
 							</div>
 						</div>
 						<div class="form-group">
 							<label>Password Baru</label>
 							<div class="input-group">
-								<input type="text" class="form-control" placeholder="" name="nama">
+								<input type="password" id="password2" class="form-control" placeholder="" name="password_baru">
+								<div class="input-group-append">
+										<div class="input-group-text"><i class="fas fa-eye " id="eyeIcon2"></i></div>
+									</div>
 							</div>
 						</div>
 						<div class="form-group">
 							<label>Konfirmasi Password Baru</label>
 							<div class="input-group">
-								<input type="text" class="form-control" placeholder="" name="nama">
+								<input type="password" id="password3" class="form-control" placeholder="" onkeyup="check()">
+								<div class="input-group-append">
+										<div class="input-group-text"><i class="fas fa-eye " id="eyeIcon3"></i></div>
+									</div>
 							</div>
+							<p id="alertPassword" class="d-flex ml-2 text-danger"></p>
 						</div>
 						<div class="form-group align-right">
-							<button type="button" class="btn btn-secondary waves-effect mr-1">Batal</button>
-							<button type="button" class="btn btn-primary waves-effect">Simpan</button>
+							<button type="cancel" name="cancel" class="btn btn-secondary waves-effect mr-1">Batal</button>
+							<button type="submit" name="submit" class="btn btn-primary waves-effect" id="btnsubmit">Simpan</button>
 						</div>
 					</form>
 				</div>
@@ -138,3 +146,83 @@
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	let passwordInput = document.getElementById('password'),
+		icon = document.getElementById('eyeIcon');
+
+	function togglePassword() {
+		if (passwordInput.type === 'password') {
+			passwordInput.type = 'text';
+			icon.classList.add("fa-eye-slash");
+			//toggle.innerHTML = 'hide';
+		} else {
+			passwordInput.type = 'password';
+			icon.classList.remove("fa-eye-slash");
+			//toggle.innerHTML = 'show';
+		}
+	}
+
+	function checkInput() {}
+
+	icon.addEventListener('click', togglePassword, false);
+	passwordInput.addEventListener('keyup', checkInput, false);
+</script>
+
+<script type="text/javascript">
+	let passwordInput2 = document.getElementById('password2'),
+		icon2 = document.getElementById('eyeIcon2');
+
+	function togglePassword2() {
+		if (passwordInput2.type === 'password') {
+			passwordInput2.type = 'text';
+			icon2.classList.add("fa-eye-slash");
+			//toggle.innerHTML = 'hide';
+		} else {
+			passwordInput2.type = 'password';
+			icon2.classList.remove("fa-eye-slash");
+			//toggle.innerHTML = 'show';
+		}
+	}
+
+	function checkInput2() {}
+
+	icon2.addEventListener('click', togglePassword2, false);
+	passwordInput2.addEventListener('keyup', checkInput2, false);
+</script>
+
+<script type="text/javascript">
+	let passwordInput3 = document.getElementById('password3'),
+		icon3 = document.getElementById('eyeIcon3');
+
+	function togglePassword3() {
+		if (passwordInput3.type === 'password') {
+			passwordInput3.type = 'text';
+			icon3.classList.add("fa-eye-slash");
+			//toggle.innerHTML = 'hide';
+		} else {
+			passwordInput3.type = 'password';
+			icon3.classList.remove("fa-eye-slash");
+			//toggle.innerHTML = 'show';
+		}
+	}
+
+	function checkInput3() {}
+
+	icon3.addEventListener('click', togglePassword3, false);
+	passwordInput3.addEventListener('keyup', checkInput3, false);
+</script>
+
+<script>
+	var check = function() {
+		if (document.getElementById('password2').value == document.getElementById('password3').value) {
+			document.getElementById('alertPassword').style.color = '#8CC63E';
+			document.getElementById('alertPassword').innerHTML = '';
+			document.getElementById('btnsubmit').disabled = false;
+		} else {
+			document.getElementById('alertPassword').style.color = '#EE2B39';
+			document.getElementById('alertPassword').innerHTML = '<span>password tidak cocok</span>';
+			document.getElementById('btnsubmit').disabled = true;
+		}
+	}
+</script>
